85204111009436d4866b006da61923eddcd2348d
Victor Chapal <vchapal@enseirb-matmeca.fr>
2018-11-16 13:04:24 +0100

    met un espace

--- light-up.sh
+++ light-up.sh
@@ -60,8 +60,61 @@ done
 
  
 ########################### Partie 3 ###########################
+#def nowall
+(define-fun nowall ((I Int) (J Int) (K Int) (L Int)) Bool
+ (for m in `seq I K`; do
+      for n in `seq J L`; do
+	  (iff not wall_${m}_{n} true)
+      done
+  done
+  ))
 
+#def haswall
+(define-fun haswall ((I Int) (J Int) (K Int) (L Int)) Bool
+ (iff not nowall(I J K L) true))
 
+#contrainte n°1
+(implies (nowall(I J K L))
+ (and (for m in `seq I K`; do
+	   for n in `seq J L`; do
+	       (islit_${m}_${n})
+	   done
+       done
+  )))
+
+#contrainte n°3
+(implies (wall_${I}_${J})
+ ((implies (or (for m in `seq 1 I`; do
+		    (bulb_${m}_${J})done))
+   (and (for m in `seq I $((N))`; do
+		    (not islit_${m}_${J})
+	 done)))
+  and
+  ((implies (or (for m in `seq I $((N))`; do
+		    (bulb_${m}_${J})done))
+   (and (for m in `seq 1 I`; do
+		    (not islit_${m}_${J})
+	 done)))
+   and
+   (implies (or (for n in `seq 1 J`; do
+		    (bulb_${I}_${n})done))
+   (and (for n in `seq J $((N))`; do
+		    (not islit_${I}_${n})
+	 done)))
+   and
+   (implies (or (for n in `seq J $((N))`; do
+		    (bulb_${I}_${n})done))
+   (and (for n in `seq 1 J`; do
+		    (not islit_${I}_${n})
+	 done))))))
+
+#contrainte n°5
+(implies (and (for I in `seq 1 $((N))`; do
+		   for J in `seq 1 $((N))`; do
+		       (wall_${I}_${J})
+		   done
+	       done))
+ (not bulb_${I}_${J}))
 
 ########################### Partie 4 ###########################
 
