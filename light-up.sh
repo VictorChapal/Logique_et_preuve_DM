6397837345095936c1e137e2f484bda7161e91a0
ClaireAubin <aubin.claire@live.fr>
2018-11-18 21:33:17 +0100

    J'en ai marre

--- light-up.sh
+++ light-up.sh
@@ -43,7 +43,8 @@ done
 for I in `seq 1 $N`; do
     for J in `seq 1 $N`; do
         if [ "${WALLS[$((I*N+J))]}" = "" ]; then
-           # echo "Pas de mur en ($I,$J)"
+            # echo "Pas de mur en ($I,$J)"
+	    echo ""
         else
             #echo "Mur en ($I,$J)  avec valeur ${WALLS[$((I*N+J))]}"
 	    #mettre cardinalite
@@ -135,45 +136,57 @@ contrainte_4_V1 # appelle la contrainte 4
 ########################### Partie 3 ###########################
 #def nowall, on l'appel en donnant en parametre I J K L
 function nowall(){
-    I=$1
-    J=$2
+    A=$1
+    B=$2
     K=$3
     L=$4
-    for M in `seq $I $K`; do
-	for P in `seq $J $L`; do
-	    echo "(assert not wall_${M}_${P})"
+    for M in `seq $A $K`; do
+	for P in `seq $B $L`; do
+	    echo "                       (assert not wall_${M}_${P})"
 	done
     done
 }
-#nowall 1 1 3 3
 
 #def haswall
 function haswall(){
-    I=$1
-    J=$2
+    A=$1
+    B=$2
     K=$3
     L=$4
+    if [ $A -gt $K ]; then
+	tmp=$A
+	A=$K
+	K=$tmp
+    fi
+    if [ $B -gt $L ]; then
+	tmp=$B
+	B=$L
+	L=$tmp
+    fi
     for M in `seq $I $K`; do
 	for P in `seq $J $L`; do
-	    echo "(assert (not"
-	    nowall $I $J $K $L
-	    echo "))"
+	    echo "                     (assert (not"
+	    nowall $A $B $K $L
+	    echo "                     ))"
 	done
     done
 }
-#haswall 1 1 3 3 
+
 
 #contrainte n°1
 function contrainte_1_V2(){
-echo"(implies (nowall(I J K L))
- (and ("
-for M in `seq I K`; do
-    for P in `seq J L`; do
-	echo"	       (islit_${M}_${P})"
+    for M in `seq 1 $((N))`; do
+	for P in `seq 1 $((N))`; do
+	    echo "(assert "
+	    echo "  (implies "
+	    echo "     (not wall_${M}_${P})"
+	    echo "     (islit_${M}_${P})"
+	    echo "  )"
+	    echo ")"
+	done
     done
-done
-echo"  )))"
 }
+contrainte_1_V2
 
 #contrainte n°3
 function contrainte_3_V2(){
@@ -241,6 +254,60 @@ function contrainte_3_V2(){
 }
 contrainte_3_V2
 
+#contrainte n°4
+function contrainte_4_V2(){
+    for I in `seq 1 $((N))`; do
+	for J in `seq 1 $((N))`; do
+	    echo "(assert
+  (implies (bulb_${I}_${J})"
+	    echo "    (and (and "
+	    for M in `seq 1 $((N))`; do
+		if [ $M -ne $I ]
+		then
+		    echo "           (not bulb_${M}_${J})"
+		fi
+	    done
+	    echo "         )"
+	    echo "         (and "
+	    for P in `seq 1 $((N))`; do
+		if [ $P -ne $J ]
+		then
+		    echo "           (not bulb_${I}_${P})"
+		fi
+	    done
+	    echo "         )"
+	    echo "    )"
+	    echo "    or("
+	    for Q in `seq 1 $((N))`; do
+		if [ $Q -ne $I ]
+		then
+		    echo "           (implies (bulb_${Q}_${J})"
+		    echo "                    ("
+		    haswall $I $J $Q $J
+		    echo "                    )"
+		    echo "           )"
+		fi
+	    done
+	    echo "    )"
+	    echo "    or("
+	    for R in `seq 1 $((N))`; do
+		if [ $R -ne $J ]
+		then
+		    echo "           (implies (bulb_${I}_${R})"
+		    echo "                    ("
+		    haswall $I $J $I $R
+		    echo "                    )"
+		    echo "           )"
+		fi
+	    done
+	    echo "    )"
+	    echo "  )"
+	    echo ")"
+	done
+    done
+}
+contrainte_4_V2
+
 #contrainte n°5
 function contrainte_5_V2(){
     for I in `seq 0 $((N+1))`; do
@@ -248,8 +315,8 @@ function contrainte_5_V2(){
 	    echo "(assert"
 	    echo "  (implies "
 	    echo "      (wall_${I}_${J})"
-	    echo "      (not bulb_${I}_${J}))"
-	    echo "    )
+	    echo "      (not bulb_${I}_${J})"
+	    echo "  )
 )"
 		   done
     done
